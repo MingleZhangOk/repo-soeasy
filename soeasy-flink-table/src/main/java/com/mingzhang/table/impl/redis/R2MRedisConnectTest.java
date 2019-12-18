d292ba3be4d0537ff9c712faf6eb4b59e3b9f927
ming <zhangming0392@163.com>
2019-12-18 22:50:56 +0800

    nothing

--- soeasy-flink-table/src/main/java/com/mingzhang/table/impl/redis/R2MRedisConnectTest.java
+++ soeasy-flink-table/src/main/java/com/mingzhang/table/impl/redis/R2MRedisConnectTest.java
@@ -5,6 +5,8 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Protocol;
 
+import javax.sound.midi.Soundbank;
+
 /**
  * File Discriptor:
  *
@@ -15,15 +17,16 @@ public class R2MRedisConnectTest {
     public static void main(String[] args) {
 
         JedisPool jedisPool = new JedisPool(new JedisPoolConfig(),
-                "",
+                "192.168.56.102",
                 6379,
                 Protocol.DEFAULT_TIMEOUT,
-                "",
-                Integer.valueOf("nams"));
+                "123456",
+                Integer.valueOf("9"));
         Jedis jedis = jedisPool.getResource();
-        String a = jedis.set("a", "13");
-        System.out.println(a);
-        String s = jedis.get("a");
-        System.out.println(s);
+        System.out.println("testIncr====" + jedis.incr("testIncr"));
+        System.out.println("putTestAdd====" + jedis.incrBy("testAdd", 100));
+        System.out.println("a=====" + jedis.set("a", "13"));
+        System.out.println("s====" + jedis.get("a"));
+        System.out.println("testAdd====" + jedis.get("testAdd"));
     }
 }
