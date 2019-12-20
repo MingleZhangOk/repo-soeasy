5e90274a93b0a8e6216f8705005d3daf9022bc2d
ming <zhangming0392@163.com>
2019-12-20 22:53:38 +0800

    nothing

--- soeasy-flink-table/src/main/java/com/mingzhang/table/impl/elastic/ElasticSearchScoll.java
+++ soeasy-flink-table/src/main/java/com/mingzhang/table/impl/elastic/ElasticSearchScoll.java
@@ -7,8 +7,10 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.Date;
 import java.util.List;
@@ -21,26 +23,52 @@ import java.util.List;
  */
 public class ElasticSearchScoll {
 
-    private static String esHost = "192.168.56.102";
+    //    private static String esHost = "172.22.4.223,172.22.4.224,172.22.4.231";
+    private static String esHost = "172.22.4.219,172.22.4.220,172.22.4.221";
     private static int esPort = 9300;
-    private static String index = ".monitoring-es-6-2019.12.06";
+    private static String index = ".monitoring-es-6-2019.12.17";
     private static String type = "doc";
     private static int size = 5000;
+    //总量331769,size =5000
+    //scroll 模式启动！耗时: 270399
+    //from size 模式启动！耗时: 470996
+
+    //总量331769,size =1000
+    //from size 模式启动！耗时: 197108
+    //scroll 模式启动！耗时: 183795
+
+    //总量331769,size =3000
+    //from size 模式启动！耗时: 173329
+    //scroll 模式启动！耗时: 172830
+
+    //总量331769,size =5000
+    //scroll 模式启动！耗时:
+    //from size 模式启动！耗时:
 
     public static void main(String[] args) throws Exception {
 
         Settings settings = Settings.builder()
                 .put("cluster.name", "my-application")
-                .put("client.transport.sniff", false)// 开启ES嗅探功能，确保集群连上多个节点
+                .put("client.transport.sniff", true)// 开启ES嗅探功能，确保集群连上多个节点
                 .build();
 
         // 创建客户端
         PreBuiltTransportClient transportClient = new PreBuiltTransportClient(settings);
         // 添加es的节点信息，可以添加1个或多个
-        InetSocketAddress inetSocketAddress = new InetSocketAddress(esHost, esPort);
+
+        String[] esHosts = esHost.split(",");
+        for (int i = 0; i < esHosts.length; i++) {
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(esHosts[i], esPort);
 //        TransportAddress transportAddress = new TransportAddress(InetAddress.getByName(esHost), esPort);
-        TransportAddress transportAddress = new TransportAddress(inetSocketAddress);
-        transportClient.addTransportAddresses(transportAddress);
+            TransportAddress transportAddress = new TransportAddress(inetSocketAddress);
+            transportClient.addTransportAddresses(transportAddress);
+        }
+
+//        InetSocketAddress inetSocketAddress = new InetSocketAddress(esHost, esPort);
+////        TransportAddress transportAddress = new TransportAddress(InetAddress.getByName(esHost), esPort);
+//        TransportAddress transportAddress = new TransportAddress(inetSocketAddress);
+//        transportClient.addTransportAddresses(transportAddress);
+
         // 连接到的节点
         List<DiscoveryNode> discoveryNodes = transportClient.connectedNodes();
         for (DiscoveryNode discoveryNode : discoveryNodes) {
